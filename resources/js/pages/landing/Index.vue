c27fb38293c181aa3737517d2d0806f748206309
Richard Hernandez <richar33333@gmail.com>
2020-06-04 19:34:32 -0400

    cambio navigation bar shopping-cart

--- resources/js/pages/landing/Index.vue
+++ resources/js/pages/landing/Index.vue
@@ -1,9 +1,16 @@
 <template>
     <v-app>
         <l-navigation-top-component />
+        <v-navigation-drawer
 
+            absolute
+            right
+            temporary
+            >
+                <h2>prueba</h2>
+        </v-navigation-drawer>
         <l-search-front-component :hotProducts=productsHot />
-
+        <h2>{{ this.$store.state.shoppingCart.cartModal }}</h2>
         <v-content>
             <router-view></router-view>
         </v-content>
@@ -14,23 +21,24 @@
 
 <script>
     import '@/plugins/global/landing'
-    import { mapGetters, mapActions } from 'vuex'
+    import { mapGetters, mapActions, mapState, mapMutations} from 'vuex'
 
     export default {
         name: 'IndexLanding',
         data() {
             return {
-                hotProducts: []
+                hotProducts: [],
             }
         },
         computed: {
-            ...mapGetters('products', ['productsHot'])
+            ...mapGetters('products', ['productsHot']),
+            ...mapMutations('shoppingCart', ['OPEN_CART_MODAL'] ),
         },
         created() {
             this.getAllProducts()
         },
         methods: {
-            ...mapActions('products', ['getAllProducts'])
+            ...mapActions('products', ['getAllProducts']),
         }
     }
 </script>
